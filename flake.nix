ca8f0c504a28d9d07ba8b659f7e68e4a8572bf4b
eureka-cpu <github.eureka@gmail.com>
2026-02-28 19:54:41 -0800

    fixes

--- flake.nix
+++ flake.nix
@@ -7,12 +7,16 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
     in
     {
       packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
         pname = "nixos-test-keyring";
         version = "0.1.0";
-        src = pkgs.lib.cleanSource ./.;
+        src = lib.cleanSourceWith {
+          filter = path: _type: !lib.hasSuffix ".nix" path;
+          src = lib.cleanSource ./.;
+        };
         cargoLock.lockFile = ./Cargo.lock;
       };
       nixosModules.default = { config, pkgs, lib, ... }: {
@@ -38,36 +42,32 @@
             self.nixosModules.default
           ];
           users.users.machine = {
-            linger = true;
             isNormalUser = true;
             home = "/home/machine";
             createHome = true;
             extraGroups = [ "wheel" ];
             password = "machine";
           };
-          users.users.root = {
-            hashedPassword = "";
-            hashedPasswordFile = null;
-          };
           services.gnome.gnome-keyring.enable = true;
-          security.pam.services = {
-            login.enableGnomeKeyring = true;
-            su.enableGnomeKeyring = true;
-          };
           services.xserver = {
             enable = true;
             updateDbusEnvironment = true;
             displayManager.startx.enable = true;
           };
-          services.getty.autologinUser = "machine";
         };
         testScript = ''
           machine.start();
-          machine.wait_for_unit("user@1000.service");
-          machine.succeed("su -l machine -c 'echo -n \"machine\" | gnome-keyring-daemon --unlock'");
-          machine.wait_for_unit("nixos-test-keyring.service", user="machine");
-          machine.sleep(4); # wait for logs to be written
-          machine.fail("su -l machine -c 'journalctl --user-unit nixos-test-keyring | grep ERROR'");
+          machine.wait_until_tty_matches("1", "login: ")
+          machine.send_chars("machine\n")
+          machine.wait_until_tty_matches("1", "Password: ")
+          machine.send_chars("machine\n")
+          machine.wait_for_unit("user@1000.service")
+          machine.wait_for_unit("nixos-test-keyring.service", user="machine")
+          machine.sleep(4) # wait for logs to be written
+          logs = machine.succeed("journalctl _SYSTEMD_USER_UNIT=nixos-test-keyring.service")
+          assert "ERROR" not in logs
+          assert "successfully set password" in logs
+          assert "successfully got password: password" in logs
         '';
       };
     };
